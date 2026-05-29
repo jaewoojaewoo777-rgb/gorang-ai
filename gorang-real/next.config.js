51b2fdac2b90d7f6cbf19313c9180a2fb09cfe9c
jaewoojaewoo777-rgb <jaewoojaewoo777@gmail.com>
2026-05-29 19:49:22 +0900

    전체 업데이트

---
+++ gorang-real/next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: { serverActions: { bodySizeLimit: '50mb' } },
+  api: { bodyParser: { sizeLimit: '50mb' } }
+}
+module.exports = nextConfig
