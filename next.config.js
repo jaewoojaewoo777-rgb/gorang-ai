3b1e237019374f8d4c944435ac71d25e9ca80f22
jaewoojaewoo777-rgb <jaewoojaewoo777@gmail.com>
2026-06-09 18:08:41 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  api: {
+    bodyParser: {
+      sizeLimit: '20mb',
+    },
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '20mb',
+    },
+  },
+}
 module.exports = nextConfig
